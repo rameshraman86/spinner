9e2a8b2aa208eb5e7f5f32c1687b6a3ea2d3a5dd
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-05 22:25:54 -0400

    reverted refactored code to old one.

--- spinner1.js
+++ spinner1.js
@@ -1,16 +1,54 @@
 process.stdout.write('hellow from spinner.... \rheyyyyy\n');
 
-const spinnerParts = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
-let timer = 100;
-
-for(let i = 0; i < spinnerParts.length; i++ ) {
-  setTimeout(() => {
-    process.stdout.write(`\r${spinnerParts[i]}   `);
-  }, timer);
-  timer += 200;
-}
 
 setTimeout(() => {
-  console.log('');
-}, timer);
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+// ... fill in the rest yourself ...
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 900);
+
+
+setTimeout(() => {
+  process.stdout.write('\r/   '); 
+}, 1100);
+
+
+setTimeout(() => {
+  process.stdout.write('\r-   '); 
+}, 1300);
+
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 1500);
+
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 1700);
+
+setTimeout(() => {
+  console.log() ;
+}, 1701);
+
+
+const spinnerParts = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+
 
