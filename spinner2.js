78e888c4ab03b178e6021190cee4df8a0b9c9b6e
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-05 22:21:55 -0400

    refactored spinner code

---
+++ spinner2.js
@@ -0,0 +1,15 @@
+process.stdout.write('hellow from spinner.... \rheyyyyy\n');
+
+const spinnerParts = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+let timer = 100;
+
+for(let i = 0; i < spinnerParts.length; i++ ) {
+  setTimeout(() => {
+    process.stdout.write(`\r${spinnerParts[i]}   `);
+  }, timer);
+  timer += 200;
+}
+
+setTimeout(() => {
+  console.log('');
+}, timer);
